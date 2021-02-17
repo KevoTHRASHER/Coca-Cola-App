ca859ef31dff9f1eea91db7d349ac6159962abcd
KevoTHRASHER <rosxhead@gmail.com>
2021-02-17 19:07:28 +0000

    User Interface create some small pieces are missing

--- BienvenidoJFrame.java
+++ BienvenidoJFrame.java
@@ -14,7 +14,7 @@ public class BienvenidoJFrame extends JFrame implements ActionListener {
 		setLayout(null);
 
 		imagenLogoCocaCola = new ImageIcon("images/logo-coca.png");
-		etiquetaLogoCocaCola = new JLabel("Sistema de Contro de Vacaciones");
+		etiquetaLogoCocaCola = new JLabel();
 		etiquetaLogoCocaCola.setBounds(30,30,300,150);
 		etiquetaLogoCocaCola.setIcon(imagenLogoCocaCola);
 		etiquetaLogoCocaCola.setForeground(Color.WHITE);
@@ -23,32 +23,33 @@ public class BienvenidoJFrame extends JFrame implements ActionListener {
 		add(etiquetaLogoCocaCola);
 
 		etiquetaSistemaControlVacaciones = new JLabel("Sistema de Control de Vacaciones");
-		etiquetaSistemaControlVacaciones.setBounds(50,180,250,30);
+		etiquetaSistemaControlVacaciones.setBounds(50,170,250,30);
 		etiquetaSistemaControlVacaciones.setForeground(Color.WHITE);
 		etiquetaSistemaControlVacaciones.setBackground(Color.RED);
 		etiquetaSistemaControlVacaciones.setOpaque(true);
 		add(etiquetaSistemaControlVacaciones);
 
 		etiquetaCapturaNombre = new JLabel("Ingrese su nombre :");
-		etiquetaCapturaNombre.setBounds(50,350,200,20);
+		etiquetaCapturaNombre.setBounds(50,250,200,25);
 		etiquetaCapturaNombre.setOpaque(true);
 		etiquetaCapturaNombre.setBackground(Color.RED);
 		etiquetaCapturaNombre.setForeground(Color.WHITE);
 		add(etiquetaCapturaNombre);
 
 		campoTextoCapturaNombre = new JTextField();
-		campoTextoCapturaNombre.setBounds(50,380,200,20);
+		campoTextoCapturaNombre.setBounds(50,280,200,25);
 		campoTextoCapturaNombre.setForeground(Color.RED);
 		add(campoTextoCapturaNombre);
 
 		botonIngresar = new JButton("Ingresar");
 		botonIngresar.setForeground(Color.RED);
-		botonIngresar.setBounds(100,420,150,30);
+		botonIngresar.setBackground(Color.WHITE);
+		botonIngresar.setBounds(90,320,120,30);
 		add(botonIngresar);
 		botonIngresar.addActionListener(this);
 
 		etiquetaMarcaRegistrada = new JLabel("2021 The Coca-Cola Company by Kevo.THRASHER");
-		etiquetaMarcaRegistrada.setBounds(50,500,250,25);
+		etiquetaMarcaRegistrada.setBounds(10,380,320,25);
 		etiquetaMarcaRegistrada.setForeground(Color.WHITE);
 		etiquetaMarcaRegistrada.setBackground(Color.RED);
 
@@ -66,7 +67,7 @@ public class BienvenidoJFrame extends JFrame implements ActionListener {
 		bienvenidoJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		bienvenidoJFrame.setTitle("Bienvenido");
 		bienvenidoJFrame.getContentPane().setBackground(Color.RED);
-		bienvenidoJFrame.setBounds(0,0,400,600);
+		bienvenidoJFrame.setBounds(0,0,350,450);
 		bienvenidoJFrame.setResizable(true);
 		bienvenidoJFrame.setLocationRelativeTo(null);
 		bienvenidoJFrame.setVisible(true);
