81046b2b05b4f4733ac28834b2cca1933824f4be
KevoTHRASHER <rosxhead@gmail.com>
2021-02-18 03:01:48 +0000

    Welcome Interface add JTextField & JLabel to LOGIN

--- BienvenidoJFrame.java
+++ BienvenidoJFrame.java
@@ -5,8 +5,8 @@ import java.awt.event.*;
 public class BienvenidoJFrame extends JFrame implements ActionListener {
 
 	private ImageIcon imagenLogoCocaCola;
-	private JLabel etiquetaLogoCocaCola, etiquetaSistemaControlVacaciones, etiquetaCapturaNombre, etiquetaMarcaRegistrada;
-	private JTextField campoTextoCapturaNombre;
+	private JLabel etiquetaLogoCocaCola, etiquetaSistemaControlVacaciones, etiquetaCapturaNombre, etiquetaMarcaRegistrada, etiquetaContraseña;
+	private JTextField campoTextoCapturaNombre, campoContraseña;
 	private JButton botonIngresar;
 
 	public BienvenidoJFrame() {
@@ -35,29 +35,43 @@ public class BienvenidoJFrame extends JFrame implements ActionListener {
 		add(etiquetaSistemaControlVacaciones);
 
 		etiquetaCapturaNombre = new JLabel("Ingrese su nombre :");
-		etiquetaCapturaNombre.setFont(new Font("Hack",1,12));
-		etiquetaCapturaNombre.setBounds(50,250,200,25);
+		etiquetaCapturaNombre.setFont(new Font("Hack",1,13));
+		etiquetaCapturaNombre.setBounds(50,210,200,25);
 		etiquetaCapturaNombre.setOpaque(true);
 		etiquetaCapturaNombre.setBackground(Color.RED);
 		etiquetaCapturaNombre.setForeground(Color.WHITE);
 		add(etiquetaCapturaNombre);
 
 		campoTextoCapturaNombre = new JTextField();
-		campoTextoCapturaNombre.setBounds(50,280,230,25);
+		campoTextoCapturaNombre.setBounds(50,240,230,25);
 		campoTextoCapturaNombre.setFont(new Font("Hack",1,14));
 		campoTextoCapturaNombre.setForeground(Color.RED);
 		add(campoTextoCapturaNombre);
 
+		etiquetaContraseña = new JLabel("Ingresa tu contraseña :");
+		etiquetaContraseña.setFont(new Font("Hack",3,13));
+		etiquetaContraseña.setBounds(50,270,200,25);
+		etiquetaContraseña.setBackground(Color.RED);
+		etiquetaContraseña.setForeground(Color.WHITE);
+		etiquetaContraseña.setOpaque(true);
+		add(etiquetaContraseña);
+
+		campoContraseña = new JTextField();
+		campoContraseña.setBounds(50,300,230,25);
+		campoContraseña.setFont(new Font("Hack",1,14));
+		campoContraseña.setForeground(Color.RED);
+		add(campoContraseña);
+
 		botonIngresar = new JButton("Ingresar");
 		botonIngresar.setForeground(Color.RED);
 		botonIngresar.setBackground(Color.WHITE);
-		botonIngresar.setBounds(100,325,120,30);
+		botonIngresar.setBounds(100,345,120,30);
 		botonIngresar.addActionListener(this);
 		add(botonIngresar);
 
 		etiquetaMarcaRegistrada = new JLabel("©2021 The Coca-Cola Company by Kevo.THRASHER");
 		etiquetaMarcaRegistrada.setFont(new Font("Hack",1,12));
-		etiquetaMarcaRegistrada.setBounds(10,380,320,25);
+		etiquetaMarcaRegistrada.setBounds(10,385,320,25);
 		etiquetaMarcaRegistrada.setForeground(Color.WHITE);
 		etiquetaMarcaRegistrada.setBackground(Color.RED);
 		add(etiquetaMarcaRegistrada);
